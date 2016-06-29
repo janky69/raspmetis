747d7d370d9273fb03d4d413a6b65726bb3dec5e
Raspberry <raspi@localhost>
2016-06-29 21:07:53 +0000

    Fetch data from Arduino

--- data_fetch.py
+++ data_fetch.py
@@ -7,15 +7,16 @@ bus = smbus.SMBus(1) # User SMBus(0) for version 1
 address = 0x25
 
 def writeNumber(value):
-	bus.write_byte(address, value)
-	# bus.write_byte_data(address, 0, value)
-	return -1
+  bus.write_byte(address, value)
+# bus.write_byte_data(address, 0, value)
+  return -1
 
 def readNumber():
-	#number = bus.read_byte(address)
-	#number = bus.read_byte_data(address, 4)
-	numbers = bus.read_i2c_block_data(address, 4)
-	return numbers
+#number = bus.read_byte(address)
+#number = bus.read_byte_data(address, 4)
+  numbers = bus.read_i2c_block_data(address, 4)
+  return numbers
+
 
 while True:
 #	var = input("Enter 1 - 9: ")
@@ -23,19 +24,20 @@ while True:
 #		continue
 #	writeNumber(var)
 #	print "RPI: Hi Arduino, I sent you", var
-	# sleep one second
-	time.sleep(1)
+# sleep one second
+  time.sleep(1)
 	
-	numbers = readNumber()
-	numbers = numbers[:4]
+  numbers = readNumber()
+  numbers = numbers[:4]
 	
-	wind_direction_c = numbers[0] + (numbers[1] << 8)
-	wind_speed = numbers[2]
-	bat = numbers[3]
-	wind_direction_p = int(wind_direction_c / 1023. * 360.)
-	wind_direction = wind_direction_p
-	if wind_direction > 180:
-		wind_direction -= 360
+  wind_direction_c = numbers[0] + (numbers[1] << 8)
+  wind_speed = numbers[2]
+  bat = numbers[3]
+  wind_direction_p = int(wind_direction_c / 1023. * 360.)
+  wind_direction = wind_direction_p
+  if wind_direction > 180:
+    wind_direction -= 360
 
 #	print "Arduino: Hey RPI, I received a digit ", number
-	print "%d -> %d -> %d" %(wind_direction_c, wind_direction_p, wind_direction)
+  print "%d -> %d -> %d" %(wind_direction_c, wind_direction_p, wind_direction)
+  print "Wind speed: %d" %(wind_speed)
