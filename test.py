d1cf1314cd95bd1cee41e2ecc0d073deb8190e76
Raspberry <raspi@localhost>
2016-06-30 12:31:42 +0000

    Add a controller for the gps

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import smbus
 import time
 from data_fetch import getData, RCP_OK, RCP_FAIL
 from data_write import DataWriter
+import gpsdData
+from gpsdData import GpsPoller
 
 bus = smbus.SMBus(1) # User SMBus(0) for version 1
 
@@ -32,23 +34,45 @@ def lcdplot(lcd_controller, data, ardu_status, gps_status):
   )
 
 if __name__ == "__main__":
+  # Setup initial statuses
+  gps_status = GPSFAIL
   lcd_status = LCDFAIL
   ardu_status = RCP_FAIL
+
+  # Initialize controllers
   lcd = LCDController()
   datawriter = DataWriter(filename="/home/pi/testdata.csv")
+  gpsp = GpsPoller()
+  gpsd = gpsdData.gpsd
+  gpsp.running = False
+  # Check how these fail in case the gps module is not connected
+  # see https://gist.github.com/wolfg1969/4653340 
+  # and https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/using-your-gps
+  # for examples
 
   while True:
     try:
       # get data from arduino
       data = getData()
       ardu_status = data[3]
+      
+      # Start the gps watcher
+      try:
+        if not gpsp.running:
+          gpsp.start()
+          gpsp.running = True
+          gps_status = GPSOK
+      except:
+        gpsp.running = False
+        gps_status = GPSFAIL
+        raise
 
       # set up the lcd
       try:
         if lcd_status == LCDFAIL:
           lcd.initialize()
           lcd_status = LCDOK
-        lcdplot(lcd, data, ardu_status, 0)
+        lcdplot(lcd, data, ardu_status, gps_status)
         #lcd.plot("Wind spd: %03d" % data[1],"Wind dir: %03d" % data[0])
 
       except IOError:
