e242b3248c1ed27782bb67a1c490b7bc9edcc8c7
luvemil <taran.marco@gmail.com>
2017-04-19 22:44:47 +0200

    Add button checker

---
+++ buttonChecker.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2.7
+# script by Alex Eames http://RasPi.tv/
+# http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio
+import RPi.GPIO as GPIO
+import time
+import threading
+
+GPIO.setmode(GPIO.BCM)
+
+# GPIO 23 set up as input. It is pulled up to stop false signals
+
+buttonPressed = None
+
+class buttonController(threading.Thread):
+  def __init__(self,buttonPin=23):
+    threading.Thread.__init__(self)
+
+    self.buttonPin = buttonPin
+    GPIO.setup(self,buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    global buttonPressed
+    buttonPressed = True
+    self.running = True
+    self.last_press = None
+
+  def run(self):
+    global buttonPressed
+    try:
+      while self.running:
+        GPIO.wait_for_edge(self.buttonPin, GPIO.FALLING)
+
+        millis = int(round(time.time() * 1000))
+        if self.last_press:
+          if self.last_press - millis > 700:
+            buttonPressed = not buttonPressed
+        self.last_press = millis
+
+      GPIO.cleanup()
+    except KeyboardInterrupt:
+      GPIO.cleanup()
+    GPIO.cleanup()
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from data_write import DataWriter
 import gpsdData
 from gpsdData import GpsPoller
 import math
+import buttonChecker
+from buttonChecker import buttonController
 
 from utils import *
 
@@ -31,8 +33,61 @@ if __name__ == '__main__':
   gpsp = GpsPoller()
   gpsd = gpsdData.gpsd
   gpsp.running = False
+  buttonc = buttonController()
+  buttonc.start()
 
   while running:
 
     running = updateRunSignal() #check if we need to shut down
-    saving = updateSaveSignal() #check if we need to save
+    saving = buttonChecker.buttonPressed #check if we need to save
+
+    try:
+
+      # get data from arduino
+      data = getData()
+      ardu_status = 1 if saving else 0 # debug value
+
+      # Start the gps watcher
+      try:
+        if not gpsp.running:
+          gpsp.running = True
+          gpsp.start()
+          gps_status = GPSOK
+      except:
+        gpsp.running = False
+        gps_status = GPSFAIL
+        raise
+
+      try:
+        adj_speed = compute_wind_speed(data[1],data[0],gpsd.speed)
+        data[1] = adj_speed
+      except:
+        pass
+
+      # set up the lcd
+      try:
+        if lcd_status == LCDFAIL:
+          lcd.initialize()
+          lcd_status = LCDOK
+        lcdplot(lcd, data, ardu_status, gps_status)
+        #lcd.plot("Wind spd: %03d" % data[1],"Wind dir: %03d" % data[0])
+
+      except IOError:
+        lcd_status = LCDFAIL
+
+      # Pass the data (formatted) to the datawriter
+      if ardu_status == RCP_OK and saving:
+        datawriter.append("%d,%d,%d"%(data[0],data[1],data[2]))
+
+      # Wait before next rount
+      time.sleep(.5)
+
+    except (KeyboardInterrupt, SystemExit):
+      if lcd_status == LCDOK:
+        lcd.plot("Quitting, bye!","")
+      gpsp.running = False
+      gpsp.join()
+      raise
+
+    except:
+      raise
